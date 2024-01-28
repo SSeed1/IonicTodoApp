f974e3f5bdf7f6d1bded01e9e99a9817246ccbd6
SSeed1 <vlad2703123@gmail.com>
2024-01-28 23:40:30 +0200

    Create Todo app section with sync in localStorage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,92 @@
 <template>
-  <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>Ionic Todo App</ion-title>
+            </ion-toolbar>
+        </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
+        <ion-content class="ion-padding">
+            <ion-list>
+                <ion-item v-for="task in tasks" :key="task.id">
+                    <ion-grid>
+                        <ion-row class="ion-align-items-center">
+                            <ion-col>{{ task.title }}</ion-col>
+                            <ion-col class="ion-text-end">
+                                <ion-button
+                                    @click="removeTask(task.id)"
+                                    color="danger"
+                                    fill="clear"
+                                >
+                                    Remove
+                                </ion-button>
+                            </ion-col>
+                        </ion-row>
+                    </ion-grid>
+                </ion-item>
+            </ion-list>
+        </ion-content>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
-  </ion-page>
+        <div class="bottom-container">
+            <ion-input
+                @IonChange="newTask = $event.target.value"
+                placeholder="Enter a new task"
+                @keyup.enter="addTask"
+            ></ion-input>
+            <ion-button @click="addTask">Add Task</ion-button>
+        </div>
+    </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-</script>
-
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+.bottom-container {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+const STORAGE_KEY = 'ionic_todo_tasks';
+
+const getTasksFromLocalStorage = () => {
+    const storedTasks = localStorage.getItem(STORAGE_KEY);
+    return storedTasks ? JSON.parse(storedTasks) : getDefaultTasks();
+};
+
+const saveTasksToLocalStorage = (tasks) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+};
+
+const getDefaultTasks = () => [
+    { id: 1, title: 'Default Task 1' },
+    { id: 2, title: 'Default Task 2' },
+];
+
+const newTask = ref('');
+const tasks = ref(getTasksFromLocalStorage());
+
+watch(tasks, (newTasks) => {
+    saveTasksToLocalStorage(newTasks);
+});
+
+const addTask = () => {
+    if (newTask.value.trim() !== '') {
+        tasks.value.push({
+            id: tasks.value.length + 1,
+            title: newTask.value,
+        });
+        newTask.value = '';
+    }
+};
+
+const removeTask = (taskId: number) => {
+    tasks.value = tasks.value.filter((task) => task.id !== taskId);
+};
+</script>
